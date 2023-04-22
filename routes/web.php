c4e8c536ab9f810191974dcf4528a9f61a0ec10d
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-22 12:53:06 +0100

    added css

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,7 +22,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+// ADMIN DASHBOARD ROUTE without admin group
+Route::get('/admin/dashboard',[AdminController::class,'dashboard']);
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
