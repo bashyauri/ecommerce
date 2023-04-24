b895e6f80fbc4bf8600cfcf8233a6ab240b3a47c
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-24 08:40:20 +0100

    populate the AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+
+use App\Models\Admin;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,11 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $adminRecords = [
+            ['id' => 1, 'name' => 'Super Admin','type' =>'superadmin','vendor_id' => 0,'mobile' => '09029991710',
+            'email' => 'superadmin@example.com','password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','image' => '','status' => 1 ],
+
+        ];
+        Admin::insert($adminRecords);
     }
 }
