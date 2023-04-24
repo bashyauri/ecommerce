19e350a666c02e8235fc033d0507a88c689b6e5d
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-24 19:58:08 +0100

    add error message to login page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -31,6 +31,14 @@
               </div>
               <h4>Hello! let's get started</h4>
               <h6 class="font-weight-light">Sign in to continue.</h6>
+              @if(Session::has('error_message'))
+              <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Error</strong> {{Session::get('error_message')}}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              @endif
               <form class="pt-3" action = "{{url('admin/login')}}" method = "POST">
                 @csrf
                 <div class="form-group">
