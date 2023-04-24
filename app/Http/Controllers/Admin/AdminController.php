18627ad662d181c8d8eb5fafc8c58a39a3a35ed7
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-24 19:50:04 +0100

    refactor the admin controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,13 +4,24 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function dashboard(){
         return view('admin.dashboard');
     }
-    public function login(){
+    public function login(Request $request){
+        if ($request->isMethod('post')){
+            $data = $request->all();
+            dd($data);
+            if(Auth::guard('admin')->attempt(['email'=>$data['email'],'password' => $data['password'],'status'=> 1])){
+                return redirect('admin/dashboard');
+            }else{
+                return redirect()->back()->with('error_message','Invalid Email or Password');
+            }
+
+        }
         return view('admin.login');
     }
 }
