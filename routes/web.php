0865a5de555089a704650ce9dba5b94a54312e1e
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-25 19:49:33 +0100

    added update password route

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,9 @@ Route::prefix('/admin')->group(function(){
 Route::match(['GET','POST'],'login',[AdminController::class,'login']);
 // ADMIN DASHBOARD
 Route::group(['middleware'=> ['admin']],function(){
+    //Update Admin Password
+    Route::match(['GET','POST'],'update-admin-password',[AdminController::class,'updateAdminPassword']);
+    //Admin Dashboard
     Route::match(['GET','POST'],'dashboard',[AdminController::class,'dashboard']);
     //Admin Logout
     Route::get('/logout',[AdminController::class,'logout']);
