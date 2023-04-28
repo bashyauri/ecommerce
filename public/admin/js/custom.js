61129169d9bf24a2ae82699a0ed73dbf92e52dc9
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-28 11:17:42 +0100

    added validation

--- public/admin/js/custom.js
+++ public/admin/js/custom.js
@@ -10,7 +10,15 @@ $(document).ready(function () {
             url: "check-admin-password",
             data: { current_password: current_password },
             success: function (response) {
-                alert(response);
+                if (response === "false") {
+                    $("#check_password").html(
+                        "<font color='red'>Current Password is incorrect!</font>"
+                    );
+                } else if (response === "true") {
+                    $("#check_password").html(
+                        "<font color='green'>Current Password is Correct!</font>"
+                    );
+                }
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log(errorThrown);
