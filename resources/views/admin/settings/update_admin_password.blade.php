5386fa4a34d40b84be4a6af5858702d7d82e7971
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-29 13:54:10 +0100

    update message alert

--- resources/views/admin/settings/update_admin_password.blade.php
+++ resources/views/admin/settings/update_admin_password.blade.php
@@ -41,6 +41,14 @@
                 </button>
               </div>
               @endif
+              @if(Session::has('success_message'))
+              <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Success</strong> {{Session::get('success_message')}}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              @endif
             <form class="forms-sample" action = "{{url('admin/update-admin-password')}}" method = "POST">
                 @csrf
                 <div class="form-group">
