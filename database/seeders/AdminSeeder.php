f993be07406584c6401c1f119f7ecc577b403f44
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-06 20:31:59 +0100

    modify admin seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -15,8 +15,8 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         $adminRecords = [
-            ['id' => 1, 'name' => 'Super Admin','type' =>'superadmin','vendor_id' => 0,'mobile' => '09029991710',
-            'email' => 'superadmin@example.com','password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','image' => '','status' => 1 ],
+            ['id' => 2, 'name' => 'Bashar','type' =>'vendor','vendor_id' => 1,'mobile' => '09029991710',
+            'email' => 'basharu@ymail.com','password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','image' => '','status' => 0 ],
 
         ];
         Admin::insert($adminRecords);
