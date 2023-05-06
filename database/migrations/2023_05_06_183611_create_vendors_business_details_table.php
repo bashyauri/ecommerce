86eb0fe4fe0336fe7b9a579dabb5a19a62a07ff5
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-06 19:49:03 +0100

    populate the migration

--- database/migrations/2023_05_06_183611_create_vendors_business_details_table.php
+++ database/migrations/2023_05_06_183611_create_vendors_business_details_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Vendor;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,21 @@ return new class extends Migration
     {
         Schema::create('vendors_business_details', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Vendor::class);
+            $table->string('shop_name');
+            $table->string('shop_address');
+            $table->string('shop_city');
+            $table->string('shop_state');
+            $table->string('shop_country');
+            $table->string('shop_pincode');
+            $table->string('shop_mobile');
+            $table->string('shop_website');
+            $table->string('shop_email');
+            $table->string('address_proof');
+            $table->string('address_proof_image');
+            $table->string('business_license_number');
+            $table->string('gst_number');
+            $table->string('pan_number');
             $table->timestamps();
         });
     }
