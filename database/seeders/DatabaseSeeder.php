8ce269d9af8f8bf6bbb437f59a610f63e8241b19
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-07 18:03:05 +0100

    populate Vendor Business Detail seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,6 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call(AdminSeeder::class);
         $this->call(VendorsTableSeeder::class);
+        $this->call(VendorsBusinessDetailsTableSeeder::class);
     }
 }
