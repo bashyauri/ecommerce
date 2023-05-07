8e753c137fdfe82b5b4687337cf260675d712ffa
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-07 18:03:33 +0100

    modify Database Seeder

--- database/seeders/VendorsBusinessDetailsTableSeeder.php
+++ database/seeders/VendorsBusinessDetailsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\VendorsBusinessDetail;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,13 @@ class VendorsBusinessDetailsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $vendorRecords = [
+            ['id' => 1,'vendor_id' => 1,'shop_name' =>'Mudal Pharmacy','shop_address' => 'Yabo, yauri','shop_city' =>'Yauri',
+            'shop_state' => 'Kebbi','shop_country' => 'Nigeria','shop_pincode' => '110001','shop_mobile'=> '09029991710',
+            'shop_website' => 'https://www.example.com','shop_email' => 'email@example.com','address_proof' => 'Passport',
+            'address_proof_image' => 'test.jpg',
+            'business_license_number' => '1111777888','gst_number' => '444111555','pan_number' => '118822211'],
+        ];
+        VendorsBusinessDetail::insert($vendorRecords);
     }
 }
