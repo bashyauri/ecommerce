d2ff7b9f4e944ec3a287b211896ec5b6f8421cde
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-07-19 23:26:55 +0100

    add relationship

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -10,5 +10,13 @@ class Admin extends Authenticable
 {
     use HasFactory;
     protected $guard = 'admin';
-
+    public function vendorPersonal(){
+        return $this->belongsTo(Vendor::class,'vendor_id');
+    }
+    public function vendorBusiness(){
+        return $this->belongsTo(VendorsBusinessDetail::class,'vendor_id');
+    }
+    public function vendorBank(){
+        return $this->belongsTo(VendorsBankDetail::class,'vendor_id');
+    }
 }
