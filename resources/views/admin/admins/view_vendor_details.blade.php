b5a608652916ef8fdeaa3064d866897f41557865
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-07-20 00:34:56 +0100

    add bank details to view

--- resources/views/admin/admins/view_vendor_details.blade.php
+++ resources/views/admin/admins/view_vendor_details.blade.php
@@ -29,7 +29,7 @@
         </div>
       </div>
 
-        <div class="row">
+      <div class="row">
         <div class="col-md-6 grid-margin stretch-card">
           <div class="card">
             <div class="card-body">
@@ -41,19 +41,19 @@
                 </div>
                 <div class="form-group">
                     <label for="vendor_name">Name</label>
-                    <input type="text" name="vendor_name" value="{{$vendorDetails->vendorPersonal->email}}" class="form-control"  readonly>
+                    <input type="text" name="vendor_name" value="{{$vendorDetails->vendorPersonal->name}}" class="form-control"  readonly>
                   </div>
                 <div class="form-group">
                     <label for="vendor_address">Address</label>
-                    <input type="text" name="vendor_address" value="{{$vendorDetails->vendorPersonal->email}}" class="form-control"  readonly>
+                    <input type="text" name="vendor_address" value="{{$vendorDetails->vendorPersonal->address}}" class="form-control"  readonly>
                 </div>
                 <div class="form-group">
                 <label for="vendor_city">City</label>
-                <input type="text" name="vendor_city" value="{{$vendorDetails->vendorPersonal->email}}" class="form-control"  readonly>
+                <input type="text" name="vendor_city" value="{{$vendorDetails->vendorPersonal->city}}" class="form-control"  readonly>
                 </div>
                 <div class="form-group">
                     <label for="vendor_state">State</label>
-                    <input type="text" name="vendor_state" value="{{$vendorDetails->state}}" class="form-control" readonly>
+                    <input type="text" name="vendor_state" value="{{$vendorDetails->vendorPersonal->state}}" class="form-control" readonly>
                 </div>
                 <div class="form-group">
                     <label for="vendor_country">Country</label>
@@ -83,8 +83,108 @@
             </div>
           </div>
         </div>
+        <div class="col-md-6 grid-margin stretch-card">
+            <div class="card">
+              <div class="card-body">
+                <h4 class="card-title">Business Information</h4>
 
 
+                  <div class="form-group">
+                      <label for="vendor_name">Shop Name</label>
+                      <input type="text" name="vendor_name" value="{{$vendorDetails->vendorBusiness->shop_name}}" class="form-control"  readonly>
+                    </div>
+                  <div class="form-group">
+                      <label for="vendor_address">Shop Address</label>
+                      <input type="text" name="vendor_address" value="{{$vendorDetails->vendorBusiness->shop_address}}" class="form-control"  readonly>
+                  </div>
+                  <div class="form-group">
+                  <label for="vendor_city">Shop City</label>
+                  <input type="text" name="vendor_city" value="{{$vendorDetails->vendorBusiness->shop_city}}" class="form-control"  readonly>
+                  </div>
+                  <div class="form-group">
+                      <label for="vendor_state">Shop State</label>
+                      <input type="text" name="vendor_state" value="{{$vendorDetails->vendorBusiness->shop_state}}" class="form-control" readonly>
+                  </div>
+                  <div class="form-group">
+                      <label for="vendor_country">Shop Country</label>
+                      <input type="text" name="vendor_country" value="{{$vendorDetails->vendorBusiness->shop_country}}" class="form-control" id="vendor_country" placeholder="Enter Country" readonly>
+                  </div>
+                  <div class="form-group">
+                      <label for="vendor_pincode">Shop PinCode</label>
+                      <input type="text" name="vendor_pincode" value="{{$vendorDetails->vendorBusiness->shop_pincode}}" class="form-control" readonly>
+                  </div>
+                  <div class="form-group">
+                    <label for="vendor_mobile">Shop Mobile</label>
+                    <input type="text" name="vendor_mobile" value="{{$vendorDetails->vendorBusiness->shop_mobile}}" class="form-control"
+                     readonly>
+                  </div>
+                  <div class="form-group">
+                    <label >Shop Website</label>
+                    <input  class="form-control" value="{{$vendorDetails->vendorBusiness->shop_website}}" readonly>
+                  </div>
+                  <div class="form-group">
+                    <label >Shop Email</label>
+                    <input  class="form-control" value="{{$vendorDetails->vendorBusiness->shop_email}}" readonly>
+                  </div>
+                  <div class="form-group">
+                    <label >Address Proof</label>
+                    <input  class="form-control" value="{{$vendorDetails->vendorBusiness->address_proof}}" readonly>
+                  </div>
+                  @if (!empty($vendorDetails->vendorBusiness->address_proof_image))
+                  <div class="form-group">
+                      <label for="vendor_image">Photo</label>
+                      <br>
+                       <img style="width:200px;" src="{{url('admin/images/proofs/'.$vendorDetails->vendorBusiness->address_proof_image)}}" >
+
+                    </div>
+
+                    @endif
+                    <div class="form-group">
+                    <label >Business License Number</label>
+                    <input  class="form-control" value="{{$vendorDetails->vendorBusiness->business_license_number}}" readonly>
+                  </div>
+                   <div class="form-group">
+                    <label >Gst Number</label>
+                    <input  class="form-control" value="{{$vendorDetails->vendorBusiness->gst_number}}" readonly>
+                  </div>
+                  <div class="form-group">
+                    <label >Pan Number</label>
+                    <input  class="form-control" value="{{$vendorDetails->vendorBusiness->pan_number}}" readonly>
+                  </div>
+
+                </form>
+              </div>
+            </div>
+          </div>
+
+
+    </div>
+    <div class="row">
+    <div class="col-md-6 grid-margin stretch-card">
+          <div class="card">
+            <div class="card-body">
+              <h4 class="card-title">Bank Information</h4>
+
+                <div class="form-group">
+                  <label >Account holder name</label>
+                  <input  class="form-control" value="{{$vendorDetails->vendorBank->account_holder_name}}" readonly>
+                </div>
+                <div class="form-group">
+                    <label for="vendor_name">Bank Name</label>
+                    <input type="text"  value="{{$vendorDetails->vendorBank->bank_name}}" class="form-control"  readonly>
+                  </div>
+                <div class="form-group">
+                    <label for="vendor_address">Account Number</label>
+                    <input type="text" value="{{$vendorDetails->vendorBank->account_number}}" class="form-control"  readonly>
+                </div>
+
+
+
+
+
+            </div>
+          </div>
+        </div>
     </div>
 
 
