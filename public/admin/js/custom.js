093c7f7b1f2cd651fd3400d6689a44b9dd040dd1
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-07-21 11:41:28 +0100

    add update status

--- public/admin/js/custom.js
+++ public/admin/js/custom.js
@@ -25,4 +25,31 @@ $(document).ready(function () {
             },
         });
     });
+    // Update Admin Status
+    $(document).on("click", ".updateAdminStatus", function () {
+        let status = $(this).children("i").attr("status");
+        let adminId = $(this).attr("admin_id");
+        $.ajax({
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+            },
+            type: "POST",
+            url: "/admin/update-admin-status",
+            data: { status: status, adminId: adminId },
+            success: function (response) {
+                if (response["status"] === 0) {
+                    $("#admin-" + adminId).html(
+                        '<i style="font-size:25px;" class="mdi mdi-bookmark-outline" status="Inactive"></i>'
+                    );
+                } else if (response["status"] === 1) {
+                    $("#admin-" + adminId).html(
+                        '<i style="font-size:25px;" class="mdi mdi-bookmark-check" status="Active"></i>'
+                    );
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            },
+        });
+    });
 });
