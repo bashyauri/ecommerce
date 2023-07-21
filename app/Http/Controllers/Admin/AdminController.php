ad4dff080bdeb161f362eefd6886ec0286076724
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-07-21 11:41:07 +0100

    add update status

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -14,11 +14,13 @@ use Intervention\Image\ImageManagerStatic as Image;
 
 class AdminController extends Controller
 {
-    public function dashboard(){
+    public function dashboard()
+    {
         return view('admin.dashboard');
     }
-    public function login(Request $request){
-        if ($request->isMethod('post')){
+    public function login(Request $request)
+    {
+        if ($request->isMethod('post')) {
             $data = $request->all();
 
             $rules = [
@@ -32,48 +34,47 @@ class AdminController extends Controller
                 'password.required' => 'Password is required!',
             ];
 
-            $this->validate($request,$rules,$customMessages);
+            $this->validate($request, $rules, $customMessages);
 
-            if(Auth::guard('admin')->attempt(['email'=>$data['email'],'password' => $data['password'],'status'=> 1])){
+            if (Auth::guard('admin')->attempt(['email' => $data['email'], 'password' => $data['password'], 'status' => 1])) {
                 return redirect('admin/dashboard');
-            }else{
-                return redirect()->back()->with('error_message','Invalid Email or Password');
+            } else {
+                return redirect()->back()->with('error_message', 'Invalid Email or Password');
             }
-
         }
         return view('admin.login');
     }
-    public function checkAdminPassword(Request $request){
+    public function checkAdminPassword(Request $request)
+    {
         $data = $request->all();
-       if(Hash::check($data['current_password'],Auth::guard('admin')->user()->password)){
-        return "true";
-
-       }
-       return "false";
+        if (Hash::check($data['current_password'], Auth::guard('admin')->user()->password)) {
+            return "true";
+        }
+        return "false";
     }
     public function updateAdminPassword(Request $request)
     {
-        if($request->isMethod('POST')){
+        if ($request->isMethod('POST')) {
             $data = $request->all();
-            if(Hash::check($data['current_password'],Auth::guard('admin')->user()->password)){
+            if (Hash::check($data['current_password'], Auth::guard('admin')->user()->password)) {
                 if ($data['confirm_password'] == $data['new_password']) {
-                    Admin::where('id',Auth::guard('admin')->user()->id)->update(['password' =>bcrypt($data['new_password'])]);
-                    return redirect()->back()->with('success_message','Password has been updated successfully');
-                } else{
-                    return redirect()->back()->with('error_message','Passwords dont match');
+                    Admin::where('id', Auth::guard('admin')->user()->id)->update(['password' => bcrypt($data['new_password'])]);
+                    return redirect()->back()->with('success_message', 'Password has been updated successfully');
+                } else {
+                    return redirect()->back()->with('error_message', 'Passwords dont match');
                 }
-
             }
-            return redirect()->back()->with('error_message','Your current password is incorrect');
+            return redirect()->back()->with('error_message', 'Your current password is incorrect');
         }
-        $adminDetails = Admin::where(['email'=>Auth::guard('admin')->user()->email])->first()->toArray();
+        $adminDetails = Admin::where(['email' => Auth::guard('admin')->user()->email])->first()->toArray();
         return view('admin.settings.update_admin_password')->with($adminDetails);
     }
-    public function updateAdminDetails(Request $request){
-        if($request->isMethod('post')){
-            $data= $request->all();
+    public function updateAdminDetails(Request $request)
+    {
+        if ($request->isMethod('post')) {
+            $data = $request->all();
 
-        //   Update Admin Details
+            //   Update Admin Details
             $rules = [
                 'admin_name' => 'required|regex:/^[\pL\s\-]+$/u',
                 'admin_mobile' => 'required|numeric'
@@ -84,35 +85,36 @@ class AdminController extends Controller
                 'admin_mobile.required' => 'Mobile is required',
                 'admin_mobile.numeric' => 'Valid Mobile is required',
             ];
-            $this->validate($request,$rules,$customMessages);
+            $this->validate($request, $rules, $customMessages);
             // Upload Photos
-            if($request->hasFile('admin_image')){
+            if ($request->hasFile('admin_image')) {
                 $image_tmp = $request->file('admin_image');
-                if($image_tmp->isValid()){
+                if ($image_tmp->isValid()) {
                     $extension = $image_tmp->getClientOriginalExtension();
-                    $imageName = rand(111,99999).'.'.$extension;
-                    $imagePath = 'admin/images/photos/'.$imageName;
+                    $imageName = rand(111, 99999) . '.' . $extension;
+                    $imagePath = 'admin/images/photos/' . $imageName;
                     Image::make($image_tmp)->save($imagePath);
-
                 }
-            } else if(!empty($data['current_admin_image'])){
+            } else if (!empty($data['current_admin_image'])) {
                 $imageName = $data['current_admin_image'];
-
             } else {
                 $imageName = "";
             }
-            Admin::where('id',Auth::guard('admin')->user()->id)->update(
-                ['name'=> $data['admin_name'],
-                'mobile' => $data['admin_mobile'],
-                'image' => $imageName]
-        );
+            Admin::where('id', Auth::guard('admin')->user()->id)->update(
+                [
+                    'name' => $data['admin_name'],
+                    'mobile' => $data['admin_mobile'],
+                    'image' => $imageName
+                ]
+            );
             return redirect()->back()->with(['success_message' => 'Admin details updated successfully!']);
         }
         return view('admin.settings.update_admin_details');
     }
-    public function updateVendorDetails(Request $request, string $slug){
-        if($slug == 'personal'){
-            if($request->isMethod('POST')){
+    public function updateVendorDetails(Request $request, string $slug)
+    {
+        if ($slug == 'personal') {
+            if ($request->isMethod('POST')) {
                 $data = $request->all();
                 $rules = [
                     'vendor_name' => 'required|regex:/^[\pL\s\-]+$/u',
@@ -126,41 +128,41 @@ class AdminController extends Controller
                     'vendor_mobile.required' => 'Mobile is required',
                     'vendor_mobile.numeric' => 'Valid Mobile is required',
                 ];
-                $this->validate($request,$rules,$customMessages);
+                $this->validate($request, $rules, $customMessages);
                 // Upload Photos
-                if($request->hasFile('vendor_image')){
+                if ($request->hasFile('vendor_image')) {
                     $image_tmp = $request->file('vendor_image');
-                    if($image_tmp->isValid()){
+                    if ($image_tmp->isValid()) {
                         $extension = $image_tmp->getClientOriginalExtension();
-                        $imageName = rand(111,99999).'.'.$extension;
-                        $imagePath = 'admin/images/photos/'.$imageName;
+                        $imageName = rand(111, 99999) . '.' . $extension;
+                        $imagePath = 'admin/images/photos/' . $imageName;
                         Image::make($image_tmp)->save($imagePath);
-
                     }
-                } else if(!empty($data['current_vendor_image'])){
+                } else if (!empty($data['current_vendor_image'])) {
                     $imageName = $data['current_vendor_image'];
-
                 } else {
                     $imageName = "";
                 }
                 // Update in Admin table
-                Admin::where('id',Auth::guard('admin')->user()->id)->update(
-                    ['name'=> $data['vendor_name'],
-                    'mobile' => $data['vendor_mobile'],
-                    'image' => $imageName]
-            );
-            // Update in vendors table
-            Vendor::where(['id'=>Auth::guard('admin')->user()->vendor_id])->update(['name'=> $data['vendor_name'],
-            'mobile' => $data['vendor_mobile'],'address' => $data['vendor_address'],
-            'city' => $data['vendor_city'],'state' => $data['vendor_state'],'country' => $data['vendor_country'],
-            'pincode' => $data['vendor_pincode'],
-            ]);
+                Admin::where('id', Auth::guard('admin')->user()->id)->update(
+                    [
+                        'name' => $data['vendor_name'],
+                        'mobile' => $data['vendor_mobile'],
+                        'image' => $imageName
+                    ]
+                );
+                // Update in vendors table
+                Vendor::where(['id' => Auth::guard('admin')->user()->vendor_id])->update([
+                    'name' => $data['vendor_name'],
+                    'mobile' => $data['vendor_mobile'], 'address' => $data['vendor_address'],
+                    'city' => $data['vendor_city'], 'state' => $data['vendor_state'], 'country' => $data['vendor_country'],
+                    'pincode' => $data['vendor_pincode'],
+                ]);
                 return redirect()->back()->with(['success_message' => 'Vendor details updated successfully!']);
             }
-            $vendorDetails = Vendor::where(['id'=>Auth::guard('admin')->user()->vendor_id])->first();
-
-        } else if($slug == 'business'){
-            if($request->isMethod('POST')){
+            $vendorDetails = Vendor::where(['id' => Auth::guard('admin')->user()->vendor_id])->first();
+        } else if ($slug == 'business') {
+            if ($request->isMethod('POST')) {
                 $data = $request->all();
                 $rules = [
                     'shop_name' => 'required|regex:/^[\pL\s\-]+$/u',
@@ -178,39 +180,37 @@ class AdminController extends Controller
                     'address_proof.required' => 'Address Proof  is required',
                     'address_proof_image.image' => 'Address Proof Image is required',
                 ];
-                $this->validate($request,$rules,$customMessages);
+                $this->validate($request, $rules, $customMessages);
                 // Upload Photos
-                if($request->hasFile('address_proof_image')){
+                if ($request->hasFile('address_proof_image')) {
                     $image_tmp = $request->file('address_proof_image');
-                    if($image_tmp->isValid()){
+                    if ($image_tmp->isValid()) {
                         $extension = $image_tmp->getClientOriginalExtension();
-                        $imageName = rand(111,99999).'.'.$extension;
-                        $imagePath = 'admin/images/proofs/'.$imageName;
+                        $imageName = rand(111, 99999) . '.' . $extension;
+                        $imagePath = 'admin/images/proofs/' . $imageName;
                         Image::make($image_tmp)->save($imagePath);
-
                     }
-                } else if(!empty($data['current_address_proof_image'])){
+                } else if (!empty($data['current_address_proof_image'])) {
                     $imageName = $data['current_address_proof_image'];
-
                 } else {
                     $imageName = "";
                 }
                 // Update in vendors_business_table_details table
-                VendorsBusinessDetail::where(['vendor_id'=>Auth::guard('admin')->user()->vendor_id])->update(['shop_name'=> $data['shop_name'],
-                'shop_mobile' => $data['shop_mobile'],'shop_address' => $data['shop_address'],
-                'shop_city' => $data['shop_city'],'shop_state' => $data['shop_state'],'shop_country' => $data['shop_country'],
-                'shop_pincode' => $data['shop_pincode'], 'business_license_number' => $data['business_license_number'],
-                'gst_number' => $data['gst_number'],'pan_number' => $data['pan_number'],
-                'address_proof' => $data['address_proof'], 'address_proof_image' => $imageName,
+                VendorsBusinessDetail::where(['vendor_id' => Auth::guard('admin')->user()->vendor_id])->update([
+                    'shop_name' => $data['shop_name'],
+                    'shop_mobile' => $data['shop_mobile'], 'shop_address' => $data['shop_address'],
+                    'shop_city' => $data['shop_city'], 'shop_state' => $data['shop_state'], 'shop_country' => $data['shop_country'],
+                    'shop_pincode' => $data['shop_pincode'], 'business_license_number' => $data['business_license_number'],
+                    'gst_number' => $data['gst_number'], 'pan_number' => $data['pan_number'],
+                    'address_proof' => $data['address_proof'], 'address_proof_image' => $imageName,
 
                 ]);
 
                 return redirect()->back()->with(['success_message' => 'Vendor details updated successfully!']);
             }
-            $vendorDetails = VendorsBusinessDetail::where(['vendor_id'=>Auth::guard('admin')->user()->vendor_id])->first();
-
-        } else if($slug == 'bank'){
-            if($request->isMethod('POST')){
+            $vendorDetails = VendorsBusinessDetail::where(['vendor_id' => Auth::guard('admin')->user()->vendor_id])->first();
+        } else if ($slug == 'bank') {
+            if ($request->isMethod('POST')) {
                 $data = $request->all();
                 $rules = [
                     'account_holder_name' => 'required|regex:/^[\pL\s\-]+$/u',
@@ -226,41 +226,53 @@ class AdminController extends Controller
                     'account_number.required' => 'Account Number is required',
 
                 ];
-                $this->validate($request,$rules,$customMessages);
+                $this->validate($request, $rules, $customMessages);
 
                 // Update in vendors_bank_table_details table
-                VendorsBankDetail::where(['vendor_id'=>Auth::guard('admin')->user()->vendor_id])->update([
-                    'account_holder_name'=> $data['account_holder_name'],
-                'bank_name' => $data['bank_name'],'account_number' => $data['account_number'],
+                VendorsBankDetail::where(['vendor_id' => Auth::guard('admin')->user()->vendor_id])->update([
+                    'account_holder_name' => $data['account_holder_name'],
+                    'bank_name' => $data['bank_name'], 'account_number' => $data['account_number'],
 
                 ]);
 
                 return redirect()->back()->with(['success_message' => 'Vendor details updated successfully!']);
             }
-            $vendorDetails = VendorsBankDetail::where(['vendor_id'=>Auth::guard('admin')->user()->vendor_id])->first();
+            $vendorDetails = VendorsBankDetail::where(['vendor_id' => Auth::guard('admin')->user()->vendor_id])->first();
         }
-        return view('admin.settings.update_vendor_details',['slug' => $slug,'vendorDetails'=>$vendorDetails]);
-
+        return view('admin.settings.update_vendor_details', ['slug' => $slug, 'vendorDetails' => $vendorDetails]);
     }
-    public function admins($type=null)
+    public function admins($type = null)
     {
         $admins = Admin::query();
-        if(!empty($type)){
-            $admins->where('type',$type);
-            $title = ucfirst($type)."s";
-        } else{
+        if (!empty($type)) {
+            $admins->where('type', $type);
+            $title = ucfirst($type) . "s";
+        } else {
             $title = "All Admins/Subadmins/Vendors";
         }
         $admins = $admins->get();
-        return view('admin.admins.admins',['admins'=>$admins, 'title'=>$title]);
-
+        return view('admin.admins.admins', ['admins' => $admins, 'title' => $title]);
     }
     public function viewVendorDetails($id)
     {
-        $vendorDetails = Admin::with('vendorPersonal','vendorBusiness','vendorBank')->where('id',$id)->first();
-        return view('admin.admins.view_vendor_details',['vendorDetails'=>$vendorDetails]);
+        $vendorDetails = Admin::with('vendorPersonal', 'vendorBusiness', 'vendorBank')->where('id', $id)->first();
+        return view('admin.admins.view_vendor_details', ['vendorDetails' => $vendorDetails]);
     }
-    public function logout(){
+    public function updateAdminStatus(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = $request->all();
+            if ($data['status'] == 'Active') {
+                $status = 0;
+            } else {
+                $status = 1;
+            }
+            Admin::where(['id' => $data['adminId']])->update(['status' => $status]);
+            return response()->json(['status' => $status, 'adminId' => $data['adminId']]);
+        }
+    }
+    public function logout()
+    {
         Auth::guard('admin')->logout();
         return redirect('admin/login');
     }
