714fb9ba26aa7bb08dba83e6a4c6c462adbc0da2
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-07-25 00:50:48 +0100

    add sections method

--- app/Http/Controllers/Admin/SectionController.php
+++ app/Http/Controllers/Admin/SectionController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Section;
 use Illuminate\Http\Request;
 
 class SectionController extends Controller
 {
-    //
+    public function sections()
+    {
+        $sections = Section::get();
+
+        return view('admin.sections.sections', ['sections' => $sections]);
+    }
 }
