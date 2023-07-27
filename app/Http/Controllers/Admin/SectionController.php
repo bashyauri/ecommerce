f16a7d808c4f4caff4a096622716f7ffdc232dc8
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-07-27 17:13:27 +0100

    add addEditFunction to Section Controller

--- app/Http/Controllers/Admin/SectionController.php
+++ app/Http/Controllers/Admin/SectionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Section;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class SectionController extends Controller
 {
@@ -27,10 +28,29 @@ class SectionController extends Controller
             return response()->json(['status' => $status, 'sectionId' => $data['sectionId']]);
         }
     }
+    public function addEditSection(Request $request, $sectionId = null)
+    {
+        Session::put('page', 'sections');
+        if (is_null($sectionId)) {
+            $title = "Add Section";
+            $section = new Section;
+            $message = "Section Added Successfully!";
+        } else {
+            $title = "Edit Section";
+            $section = Section::find($sectionId);
+            $message = "Section Updated Successfully!";
+        }
+        if ($request->isMethod('POST')) {
+            $data = $request->all();
+            dd($data);
+        }
+        return view('admin.sections.add_edit_section', ['title' => $title, 'section' => $section, 'message' => $message]);
+    }
     public function deleteSection($id)
     {
         // Delete Section
         Section::where(['id' => $id])->delete();
+
 
         return redirect()->back()->with(['success_message' => 'Section deleted successfully!']);
     }
